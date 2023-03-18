5f14e15c91a6ef8f47403a476d2bd27145cf511e
Eduard <edwardbayron@gmail.com>
2023-03-18 16:26:25 +0200

    added fab icon into Column of the list of items

--- app/src/main/java/com/paybrother/MainActivity.kt
+++ app/src/main/java/com/paybrother/MainActivity.kt
@@ -3,20 +3,23 @@ package com.paybrother
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.paybrother.compose.LoanElementView
 import com.paybrother.ui.theme.MeetimeApp_v3Theme
 import com.paybrother.viewmodels.LoanViewModel
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
 
@@ -34,6 +37,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     HomeContainer(viewModel!!)
+
+
                 }
             }
         }
@@ -45,6 +50,7 @@ fun HomeContainer(viewModel: LoanViewModel) {
     Column {
         AppBarView()
         HomeDataContainer(viewModel)
+
 
     }
 }
@@ -80,6 +86,22 @@ fun HomeDataContainer(viewModel: LoanViewModel){
         }
 
 
+        Box(Modifier.fillMaxSize()){
+            FloatingActionButton(
+                modifier = Modifier.align(alignment = Alignment.BottomEnd).padding(bottom = 10.dp, end = 10.dp),
+                onClick = {
+                    //OnClick Method
+                },
+                containerColor = Color.Red,
+                shape = RoundedCornerShape(16.dp),
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = "Add FAB",
+                    tint = Color.White,
+                )
+            }
+        }
 
     }
 }
